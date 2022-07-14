1b25f205b88ebc208206fbb4f20ad7728d9b352c
Chynnie <okekechinenye92@gmail.com>
2022-07-14 17:25:36 +0100

    React Project Progress

--- GraphQL/server.js
+++ GraphQL/server.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const expressGraphQL = require('express-graphql').graphqlHTTP;
 const {
@@ -19,150 +20,223 @@ const users = [
 const transactions = [
 	{
 		id: 1,
-		transactionType: 'Debit',
-		amount: 50000,
-		status: 'Successful',
-		userId: 2,
+		date: {
+			username: 'Gbenga Oluwole',
+			month: 'January',
+			transactionType: 'Debit',
+			amount: 50000,
+			status: 'Successful',
+			userId: 2,
+		},
 	},
 	{
 		id: 2,
-		transactionType: 'Debit',
-		amount: 10000,
-		status: 'Not Successful',
-		userId: 4,
+		date: {
+			username: 'Kevwe Ovie',
+			month: 'January',
+			transactionType: 'Debit',
+			amount: 10000,
+			status: 'Not Successful',
+			userId: 4,
+		},
 	},
 	{
 		id: 3,
-		transactionType: 'Credit',
-		amount: 200000,
-		status: 'Pending',
-		userId: 3,
+		date: {
+			username: 'Hashim Yakubu',
+			month: 'January',
+			transactionType: 'Credit',
+			amount: 200000,
+			status: 'Pending',
+			userId: 3,
+		},
 	},
 	{
 		id: 4,
-		transactionType: 'Credit',
-		amount: 30000,
-		status: 'Successful',
-		userId: 1,
+		date: {
+			username: 'Gabriel Okoye',
+			month: 'January',
+			transactionType: 'Credit',
+			amount: 30000,
+			status: 'Successful',
+			userId: 1,
+		},
 	},
 	{
 		id: 5,
-		transactionType: 'Debit',
-		amount: 100000,
-		status: 'Pending',
-		userId: 4,
+		date: {
+			username: 'Kevwe Ovie',
+			month: 'February',
+			transactionType: 'Debit',
+			amount: 100000,
+			status: 'Pending',
+			userId: 4,
+		},
 	},
 	{
 		id: 6,
-		transactionType: 'Credit',
-		amount: 500000,
-		status: 'Successful',
-		userId: 2,
+		date: {
+			username: 'Gbenga Oluwole',
+			month: 'February',
+			transactionType: 'Credit',
+			amount: 500000,
+			status: 'Successful',
+			userId: 2,
+		},
 	},
 	{
 		id: 7,
-		transactionType: 'Credit',
-		amount: 3000000,
-		status: 'Not Successful',
-		userId: 1,
+		date: {
+			username: 'Gabriel Okoye',
+			month: 'February',
+			transactionType: 'Credit',
+			amount: 3000000,
+			status: 'Not Successful',
+			userId: 1,
+		},
 	},
 	{
 		id: 8,
-		transactionType: 'Debit',
-		amount: 40000,
-		status: 'Pending',
-		userId: 3,
+		date: {
+			username: 'Hashim Yakubu',
+			month: 'February',
+			transactionType: 'Debit',
+			amount: 40000,
+			status: 'Pending',
+			userId: 3,
+		},
 	},
 	{
 		id: 9,
-		transactionType: 'Credit',
-		amount: 300000,
-		status: 'Successful',
-		userId: 3,
+		date: {
+			username: 'Hashim Yakubu',
+			month: 'March',
+			transactionType: 'Credit',
+			amount: 300000,
+			status: 'Successful',
+			userId: 3,
+		},
 	},
 	{
 		id: 10,
-		transactionType: 'Debit',
-		amount: 150000,
-		status: 'Pending',
-		userId: 1,
+		date: {
+			username: 'Gabriel Okoye',
+			month: 'March',
+			transactionType: 'Debit',
+			amount: 150000,
+			status: 'Pending',
+			userId: 1,
+		},
 	},
 	{
 		id: 11,
-		transactionType: 'Credit',
-		amount: 5000,
-		status: 'Successful',
-		userId: 2,
+		date: {
+			username: 'Gbenga Oluwole',
+			month: 'March',
+			transactionType: 'Credit',
+			amount: 5000,
+			status: 'Successful',
+			userId: 2,
+		},
 	},
 	{
 		id: 12,
-		transactionType: 'Credit',
-		amount: 15000,
-		status: 'Successful',
-		userId: 4,
-	},
-	{
-		id: 12,
-		transactionType: 'Credit',
-		amount: 20000,
-		status: 'Not SUccessful',
-		userId: 1,
+		date: {
+			username: 'Kevwe Ovie',
+			month: 'March',
+			transactionType: 'Credit',
+			amount: 15000,
+			status: 'Successful',
+			userId: 4,
+		},
 	},
 	{
 		id: 13,
-		transactionType: 'Debit',
-		amount: 10000,
-		status: 'Successful',
-		userId: 2,
+		date: {
+			username: 'Gbenga Oluwole',
+			month: 'April',
+			transactionType: 'Debit',
+			amount: 10000,
+			status: 'Successful',
+			userId: 2,
+		},
 	},
 	{
 		id: 14,
-		transactionType: 'Debit',
-		amount: 40000,
-		status: 'Successful',
-		userId: 3,
+		date: {
+			username: 'Hashim Yakubu',
+			month: 'April',
+			transactionType: 'Debit',
+			amount: 40000,
+			status: 'Successful',
+			userId: 3,
+		},
 	},
 	{
 		id: 15,
-		transactionType: 'Credit',
-		amount: 80000,
-		status: 'Successful',
-		userId: 4,
+		date: {
+			username: 'Kevwe Ovie',
+			month: 'April',
+			transactionType: 'Credit',
+			amount: 80000,
+			status: 'Successful',
+			userId: 4,
+		},
 	},
 	{
 		id: 16,
-		transactionType: 'Credit',
-		amount: 50000,
-		status: 'Successful',
-		userId: 2,
+		date: {
+			username: 'Gbenga Oluwole',
+			month: 'April',
+			transactionType: 'Credit',
+			amount: 50000,
+			status: 'Successful',
+			userId: 2,
+		},
 	},
 	{
 		id: 17,
-		transactionType: 'Debit',
-		amount: 5000,
-		status: 'Pending',
-		userId: 4,
+		date: {
+			username: 'Kevwe Ovie',
+			month: 'May',
+			transactionType: 'Debit',
+			amount: 5000,
+			status: 'Pending',
+			userId: 4,
+		},
 	},
 	{
 		id: 18,
-		transactionType: 'Credit',
-		amount: 100000,
-		status: 'Not Successful',
-		userId: 3,
+		date: {
+			username: 'Hashim Yakubu',
+			month: 'May',
+			transactionType: 'Credit',
+			amount: 100000,
+			status: 'Not Successful',
+			userId: 3,
+		},
 	},
 	{
 		id: 19,
-		transactionType: 'Credit',
-		amount: 60000,
-		status: 'Pending',
-		userId: 1,
+		date: {
+			username: 'Gabriel Okoye',
+			month: 'May',
+			transactionType: 'Credit',
+			amount: 60000,
+			status: 'Pending',
+			userId: 1,
+		},
 	},
 	{
 		id: 20,
-		transactionType: 'Credit',
-		amount: 200000,
-		status: 'Successful',
-		userId: 3,
+		date: {
+			username: 'Gabriel Okoye',
+			month: 'May',
+			transactionType: 'Credit',
+			amount: 200000,
+			status: 'Successful',
+			userId: 1,
+		},
 	},
 ];
 
@@ -180,10 +254,20 @@ const TransactionType = new GraphQLObjectType({
 	description: "This API represents the summary of a user's transactions",
 	fields: () => ({
 		id: { type: new GraphQLNonNull(GraphQLInt) },
-		transactionType: { type: new GraphQLNonNull(GraphQLString) },
-		amount: { type: new GraphQLNonNull(GraphQLInt) },
-		status: { type: new GraphQLNonNull(GraphQLString) },
-		userId: { type: new GraphQLNonNull(GraphQLInt) },
+		date: {
+			type: new GraphQLObjectType({
+				name: 'TransactionDate',
+				description: 'This API represents the duration of transactions made',
+				fields: () => ({
+					username: { type: new GraphQLNonNull(GraphQLString) },
+					month: { type: new GraphQLNonNull(GraphQLString) },
+					transactionType: { type: new GraphQLNonNull(GraphQLString) },
+					amount: { type: new GraphQLNonNull(GraphQLInt) },
+					status: { type: new GraphQLNonNull(GraphQLString) },
+					userId: { type: new GraphQLNonNull(GraphQLInt) },
+				}),
+			}),
+		},
 		user: {
 			type: UserType,
 			resolve: (transaction) => {
@@ -213,6 +297,28 @@ const RootTransactionType = new GraphQLObjectType({
 const schema = new GraphQLSchema({
 	query: RootTransactionType,
 });
+
+app.all('/*', function (req, res, next) {
+	// CORS headers
+	res.header('Access-Control-Allow-Origin', '*'); // restrict it to the required domain
+	res.header('Access-Control-Allow-Credentials', 'true');
+	res.header(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+	);
+	// Set custom headers for CORS
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Access-Control-Allow-Headers, save-path, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, X-Access-Token, X-Key'
+	);
+	if (req.method == 'OPTIONS') {
+		res.status(200).end();
+	} else {
+		next();
+	}
+});
+
+app.use(cors());
 
 app.use(
 	'/graphql',
